36284a375c1e1e90739697deadd4f2e6fb1afa44
kali-pankaj <kali620162016201@gmail.com>
2026-04-23 08:50:54 +0530

    Update minikube.groovy

--- vars/minikube.groovy
+++ vars/minikube.groovy
@@ -1,10 +1,16 @@
-#!/bin/bash
-set -e 
-echo "minikube stop and delete"
-minikube delete
+def call() {
+    sh """
+        echo "Checking Minikube status..."
 
-echo "minikube start"
-minikube start --cpus=2 --memory=4096
+        minikube status 
 
-miinkube status
-echo "minikube start successful.."
+        echo "minikube start "
+        minikube start --cpus=2 --memory=4096
+
+        // kubectl apply -f kubernetes/my-webapp/
+
+        // kubectl get pods -o wide
+        minikube status 
+        echo "minikube starte successful..."
+    """
+}
